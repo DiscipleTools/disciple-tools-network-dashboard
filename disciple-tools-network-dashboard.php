d53c07cdf700ad82713873b8ac8e192578fe3211
Chris Chasm <chris@chasm.solutions>
2019-03-25 12:03:42 -0600

    Made plugin updater dependent on DT source.

--- disciple-tools-network-dashboard.php
+++ disciple-tools-network-dashboard.php
@@ -210,8 +210,8 @@ class DT_Network_Dashboard {
     private function setup_actions() {
 
         // Check for plugin updates
-        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-            require( $this->admin_path . 'libraries/plugin-update-checker/plugin-update-checker.php' );
+        if ( ! class_exists( 'Puc_v4p5_Factory' ) ) {
+            require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
         }
         /**
          * Below is the publicly hosted .json file that carries the version information. This file can be hosted
@@ -220,7 +220,7 @@ class DT_Network_Dashboard {
          * Also, see the instructions for version updating to understand the steps involved.
          * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
          */
-        Puc_v4_Factory::buildUpdateChecker(
+        Puc_v4p5_Factory::buildUpdateChecker(
             'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-version-control/master/disciple-tools-network-dashboard-version-control.json',
             __FILE__,
             'disciple-tools-network-dashboard'
