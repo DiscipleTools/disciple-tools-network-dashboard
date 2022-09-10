e62b93a1579149df1c5cefd27eed694d110adac8
Chris Chasm <chris@chasm.solutions>
2022-09-10 15:17:35 -0600

    Bump to 2.9

--- disciple-tools-network-dashboard.php
+++ disciple-tools-network-dashboard.php
@@ -5,12 +5,12 @@
  * Description: Connect this Disciple.Tools site to a larger network of sites. Adds security sensitive totals, mapping, activity logging.
  * Text Domain: disciple-tools-network-dashboard
  * Domain Path: /languages
- * Version: 2.8
+ * Version: 2.9
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/disciple-tools-network-dashboard
  * Requires at least: 4.7.0
  * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
- * Tested up to: 5.6
+ * Tested up to: 6.1
  * @package Disciple_Tools
  * @link    https://github.com/DiscipleTools
  * @license GPL-2.0 or later
